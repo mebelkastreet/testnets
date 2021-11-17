20a36604abea693bceae13ae82fd5381759c683a
Prajjwol Gautam <not@acyb.org>
2021-11-17 10:42:37 -0800

    ci: update genesis.json

--- scripts/validate-gentxs.sh
+++ scripts/validate-gentxs.sh
@@ -76,6 +76,8 @@ if [ "$(ls -A $GENTXS_DIR)" ]; then
                 exit 1
             fi
 
+            cat $DAEMON_HOME/config/genesis.json | jq
+
             $DAEMON add-genesis-account $RANDOM_KEY 1000000000000000$DENOM --home $DAEMON_HOME \
                 --keyring-backend test
 
